2291e07ffa53212b573eb879f0def97826cfde4d
kimchaeyeon <chennywa@naver.com>
2019-02-17 17:33:04 +0900

    upload SimpleDiv

---
+++ algorithm_pratice/algorithm_pratice/src/warmup/Regitration.java
@@ -0,0 +1,11 @@
+package warmup;
+
+public class Regitration {
+
+	public static void main(String[] args) {
+		System.out.println("7");
+		System.out.println("chennywa");
+		
+	}
+
+}
---
+++ algorithm_pratice/algorithm_pratice/src/warmup/SimpleMul.java
@@ -0,0 +1,16 @@
+package warmup;
+
+import java.util.Scanner;
+
+public class SimpleMul {
+
+	public static void main(String[] args) {
+		int a=0,b=0;
+		Scanner kb = new Scanner(System.in);
+		a = kb.nextInt();
+		b = kb.nextInt();
+		kb.close();
+		System.out.println(a * b);
+	}
+
+}
