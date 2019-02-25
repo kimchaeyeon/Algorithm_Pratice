cff76c81f827f664bbaac25f65cc07ed4325e3db
kimchaeyeon <chennywa@naver.com>
2019-02-25 14:36:51 +0900

    upload test

---
+++ algorithm_pratice/algorithm_pratice/src/CompareTheTriplets.java
@@ -0,0 +1,34 @@
+
+
+import java.io.*;
+import java.util.Scanner;
+
+public class CompareTheTriplets{
+
+    public static void main(String[] args) {
+      Scanner kb = new Scanner(System.in);
+      int a[] = new int[3];
+      for(int i=0; i<3; i++){
+    	  a[i]=kb.nextInt();
+      }
+      int b[] = new int[3];
+      for(int i=0; i<3; i++){
+    	  b[i]=kb.nextInt();
+      }
+      compare(a,b);
+    }
+
+	private static void compare(int[] a, int[] b) {
+		if (a[i] > b[i]){
+			
+		}
+		else if (a[i] b <[i]){
+			
+		}
+		else if (a[i] = b[i]){
+			
+		}
+	}
+
+	
+}
--- algorithm_pratice/algorithm_pratice/src/warmup/Main.java
+++ algorithm_pratice/algorithm_pratice/src/warmup/Main.java
@@ -1,20 +1,10 @@
 package warmup;
 
-import java.util.Scanner;
-
 public class Main {
+
 	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
-			int n = kb.nextInt();
-			kb.close();
-			for(int i=n; i>=1; i--){
-				for(int z=0; z<n-i; z++){
-					System.out.print(" ");
-				}
-				for(int j=0; j<i; j++){
-					System.out.print("*");
-				}
-				System.out.println("");
-			}
+		// TODO Auto-generated method stub
+
 	}
+
 }
